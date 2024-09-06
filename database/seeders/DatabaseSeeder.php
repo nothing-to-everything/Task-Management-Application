881c4ae7f98be197b8aab9ecded6fd5445b27464
NEO-Home <nothingmakessense6969@gmail.com>
2024-09-06 13:26:02 +0530

    database seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
