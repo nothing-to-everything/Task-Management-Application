f3359b2923621606d62cfe5f1166f81220ecca81
NEO-Home <nothingmakessense6969@gmail.com>
2024-09-06 09:44:24 +0530

    User Login routes declared

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,19 @@ Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->na
 
 // Handle registration submission
 Route::post('/register', [RegisterController::class, 'create']);
+
+// Show login form
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+
+// Handle login request
+Route::post('/login', [LoginController::class, 'login']);
+
+// Logout route
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Authenticated Users Can Access These Routes
+Route::middleware('auth')->group(function () {
+    Route::resource('tasks', TaskController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+
