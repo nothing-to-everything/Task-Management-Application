d7a32e0956e66994ea00897926321976d5e4160b
NEO-Home <nothingmakessense6969@gmail.com>
2024-09-06 09:39:35 +0530

    User Registration routes declared

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Show registration form
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+
+// Handle registration submission
+Route::post('/register', [RegisterController::class, 'create']);
